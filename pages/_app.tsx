8784a405deadf46b718e571617e1309705ddb2b6
mannguku <110129154+mannguku@users.noreply.github.com>
2022-10-05 21:37:24 +0300

    update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import 'node_modules/'
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
